9c1af590b08a461b915c176ad47f1e003eab7deb
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-03-07 16:41:15 +0300

    add work.html file and content

---
+++ dist/js/home.js
@@ -0,0 +1,35 @@
+// const paraOne=document.querySelector('#paragraph1');
+// const paraTwo=document.querySelector('#paragraph2');
+// const paraThree=document.querySelector('#paragraph3');
+// let timer = setInterval(changePara,2000);
+
+// let totalParas=[paraOne,paraTwo,paraThree];
+
+// // function changePara(){
+    
+// // }
+
+
+
+
+// // let currentPara = 1;
+// // let timer = setInterval(changePara,4000);
+// // let maxNumOfParas = 3;
+
+// // function changePara(){
+// //     if(currentPara<maxNumOfParas.length){
+// //         console.log('timer')
+// //         // let currentElement=document.getElementById('paragraph'+currentPara);
+// //         // currentElement.classList.add("hide");
+// //         // let nextPara=document.getElementById('paragraph'+(currentPara+1));
+// //         // nextPara.classList.remove("hide");
+// //         // currentPara+=1;
+// //     }
+// //     // else if(currentPara=maxNumOfParas){
+// //     //     // clearInterval(timer);
+// //     //     let previousPara=nextPara
+// //     //     console.log('ok')
+
+// //     // }
+// // } 
+// // changePara()
