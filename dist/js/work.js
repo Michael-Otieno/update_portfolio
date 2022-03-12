6967557195621a72f50d826a97a54902d427585f
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-03-12 16:04:23 +0300

    add hover on work image

--- dist/js/work.js
+++ dist/js/work.js
@@ -2,7 +2,5 @@ const projectImage=document.querySelector('.project-image');
 const hoverText=document.querySelector('.project-hover');
 
 projectImage.addEventListener('mouseenter',()=>{
-    hoverText.classList.remove('hide')
-    hoverText.classList.add('hide')
-
+    console.log('mouse')
 })
