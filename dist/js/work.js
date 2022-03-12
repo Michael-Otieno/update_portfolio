8c0f740e7ed6f3148ab62931a648c0e0f0f18cf8
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-03-12 16:05:57 +0300

    delete work.js

--- dist/js/work.js
+++
@@ -1,6 +0,0 @@
-const projectImage=document.querySelector('.project-image');
-const hoverText=document.querySelector('.project-hover');
-
-projectImage.addEventListener('mouseenter',()=>{
-    console.log('mouse')
-})
