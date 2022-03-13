2e6dd134c6db0c575997d02ec54fc74a8067be7c
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-03-13 17:46:20 +0300

    add style to footer icon responsive

--- dist/js/footer.js
+++ dist/js/footer.js
@@ -3,10 +3,10 @@ const createFooter = () => {
 
     footer.innerHTML = `
     <div class="footer-icons">
-        <img src="./img/icons/linkedin-brands.svg" alt="" class="footer-icons_one">
-        <img src="./img/icons/linkedin-brands.svg" alt=""  class="footer-icons_two">
-        <img src="./img/icons/github-brands.svg" alt="" class="footer-icons_three">
-        <img src="./img/icons/github-brands.svg" alt="" class="footer-icons_four">
+        <img src="./img/icons/linkedin-brands.svg" alt="" class="footer-icons">
+        <img src="./img/icons/linkedin-brands.svg" alt=""  class="footer-icons">
+        <img src="./img/icons/github-brands.svg" alt="" class="footer-icons">
+        <img src="./img/icons/github-brands.svg" alt="" class="footer-icons">
     </div>
     <p class="footer-copyright">&copy;2022 Michael Otieno</p>
 
