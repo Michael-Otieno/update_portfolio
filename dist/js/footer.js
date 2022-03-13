d9a338b2d848f9b8377563a8d98819546f996363
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-03-13 19:50:17 +0300

    add links to footer icons

--- dist/js/footer.js
+++ dist/js/footer.js
@@ -3,9 +3,16 @@ const createFooter = () => {
 
     footer.innerHTML = `
     <div class="footer-icons">
-        <img src="./img/icons/linkedin-brands.svg" alt="" class="footer-icons">
-        <img src="./img/icons/github-brands.svg" alt="" class="footer-icons">
-        <img src="./img/icons/twitter-square-brands.svg" alt=""  class="footer-icons">
+        <a href="https://www.linkedin.com/in/michael-otieno-924157217/" target="_blank">
+            <img src="./img/icons/linkedin-brands.svg" alt="" class="footer-icons">
+        </a>
+
+        <a href="http://" target="_blank">
+            <img src="./img/icons/github-brands.svg" alt="" class="footer-icons">
+        </a>
+        <a href="http://" target="_blank">
+            <img src="./img/icons/twitter-square-brands.svg" alt=""  class="footer-icons">
+        </a>
     </div>
     <p class="footer-copyright">&copy;2022 Michael Otieno</p>
 
