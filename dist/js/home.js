d67f559c8c32ae327512d0a2497b5d72fe07485e
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-03-13 09:22:07 +0300

    add homepage animatios

--- dist/js/home.js
+++ dist/js/home.js
@@ -1,35 +1,65 @@
-// const paraOne=document.querySelector('#paragraph1');
-// const paraTwo=document.querySelector('#paragraph2');
-// const paraThree=document.querySelector('#paragraph3');
-// let timer = setInterval(changePara,2000);
+var slideIndex = 1;
+showSlides(slideIndex);
 
-// let totalParas=[paraOne,paraTwo,paraThree];
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
 
-// // function changePara(){
-    
-// // }
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("home-tag");
+  var dots = document.getElementsByClassName("home-hamburger_icon");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
 
 
 
 
-// // let currentPara = 1;
-// // let timer = setInterval(changePara,4000);
-// // let maxNumOfParas = 3;
 
-// // function changePara(){
-// //     if(currentPara<maxNumOfParas.length){
-// //         console.log('timer')
-// //         // let currentElement=document.getElementById('paragraph'+currentPara);
-// //         // currentElement.classList.add("hide");
-// //         // let nextPara=document.getElementById('paragraph'+(currentPara+1));
-// //         // nextPara.classList.remove("hide");
-// //         // currentPara+=1;
-// //     }
-// //     // else if(currentPara=maxNumOfParas){
-// //     //     // clearInterval(timer);
-// //     //     let previousPara=nextPara
-// //     //     console.log('ok')
 
-// //     // }
-// // } 
-// // changePara()
+// var slideIndex=1;
+// showSlides(slideIndex);
+
+// function plusSlides(n){
+//     showSlides((slideIndex+=n));
+// }
+// function currentSlide(n){
+//     showSlides((slideIndex=n));
+// }
+
+// function showSlides(n){
+//     var i;
+//     var slides=document.getElementsByClassName('home-tag');
+//     var hamburger=document.getElementsByClassName('home-hamburger_icon');
+//     if (n>slides.length){
+//         slideIndex=1;
+//     }
+//     if(n<1){
+//         slideIndex=slides.length;
+//     }
+//     for(i=0;i<slides.length; i++){
+//         slides[i].style.display='none';
+//     }
+//     for(i=0;i<hamburger.length; i++){
+//         hamburger[i].className=hamburger[i].className.replace("active", "");
+//     }
+//     slides[slideIndex-1].style.display="block";
+//     hamburger[slideIndex-1].className+='active';
+// }
