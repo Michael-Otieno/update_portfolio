d19afc97c9f1a4dabf1ea2326fd016d461b96a65
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-05-11 11:45:52 +0300

    add new project

--- dist/js/footer.js
+++ dist/js/footer.js
@@ -4,14 +4,14 @@ const createFooter = () => {
     footer.innerHTML = `
     <div class="footer-icons">
         <a href="https://www.linkedin.com/in/michael-otieno-924157217/" target="_blank">
-            <img src="./img/icons/linkedin-brands.svg" alt="" class="footer-icons">
+            <img src="./dist/img/icons/linkedin-brands.svg" alt="" class="footer-icons">
         </a>
 
         <a href="https://github.com/Michael-Otieno/" target="_blank">
-            <img src="./img/icons/github-brands.svg" alt="" class="footer-icons">
+            <img src="./dist/img/icons/github-brands.svg" alt="" class="footer-icons">
         </a>
         <a href="https://twitter.com/justDoCode" target="_blank">
-            <img src="./img/icons/twitter-square-brands.svg" alt=""  class="footer-icons">
+            <img src="./dist/img/icons/twitter-square-brands.svg" alt=""  class="footer-icons">
         </a>
     </div>
     <p class="footer-copyright">&copy;2022 Michael Otieno</p>
